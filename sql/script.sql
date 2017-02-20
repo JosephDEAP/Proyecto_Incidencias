efac1f987379e790b0f7b84e3f779bcc064c765f
Juank <jromanamador@gmail.com>
2017-02-20 17:30:15 +0100

    Add folder and file E/R

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,14 @@
 /* 20/02/2017 */
+
+/*Secciones*/
+
+create table Secciones
+(IdSecciones char (5) PRIMARY KEY,
+ NombreSecciones varchar (60) not null,
+ IdTutor tinyint not null,
+ IdEtapa tinyint  not null,
+  constraint clave_tut
+  foreign key (IdTutor) references Profesores (IdProfesor),
+  constraint fk_dept_id
+  foreign key (IdEtapa) references Etapas (IdEtapa)
+);
