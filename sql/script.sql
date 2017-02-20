b49a43eceaff9850ca91109349c7f6a3dd1ed412
Rafa <rafapacense@gmail.com>
2017-02-20 21:04:46 +0100

    ADD SCRIPT TABLA-4,13,17

--- sql/script.sql
+++ sql/script.sql
@@ -1,2 +1,30 @@
 /* 20/02/2017 */
-ahadgbg
+
+/* TABLA 4-ALUMNOS */
+CREATE TABLE  IF NOT EXISTS alumnos(
+  nia CHAR (7) PRIMARY KEY,
+  nombreCompleto VARCHAR(50) NOT NULL,
+  idSeccion CHAR(6) NOT NULL ,
+  CONSTRAINT fk_secciones_alumnos  FOREIGN KEY (idSeccion) REFERENCES secciones(idSeccion)
+);
+
+/* TABLA 13-ANOTACIONES */
+CREATE TABLE IF NOT EXISTS anotaciones(
+  numAnotacion SMALLINT PRIMARY KEY,
+  tipoAnotacion TINYINT NOT NULL,
+  nia CHAR(7) NOT NULL ,
+  hora_Registro DATETIME NOT NULL,
+  userCreacion CHAR(1) NOT NULL ,
+  leida BIT(1) NOT NULL,
+  CONSTRAINT anotaciones_1 FOREIGN KEY (tipoAnotacion)
+  REFERENCES tipos_anotaciones_etapas (tipoAnotacion),
+  CONSTRAINT anotaciones_2 FOREIGN KEY (nia) REFERENCES alumnos(nia)
+
+  /* TABLA 17-GESTION */
+);
+CREATE TABLE IF NOT EXISTS gestion(
+  idUsuario TINYINT PRIMARY KEY,
+  nombre VARCHAR(20) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  tipo CHAR(1) NOT NULL
+);
